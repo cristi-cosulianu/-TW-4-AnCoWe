f277b64a3b1bc8dd05e183f508b6679cd189a261
cristi-cosulianu <cristi_cosulianu@yahoo.com>
2018-03-15 21:30:50 +0200

    Add files via upload

--- magicWebsite.js
+++ magicWebsite.js
@@ -1,4 +1,18 @@
 function setDisplayNone(firstId,secondId){
-    document.getElementById(firstId).style.display="none";
-    document.getElementById(secondId).style.display="block";
+    fade(firstId);
+    
+    setTimeout(function(){
+        document.getElementById(firstId).style.display="none";
+        document.getElementById(secondId).style.display="block";
+        setTimeout(function(){
+            fade(secondId);
+        }, 1000);
+    }, 1000);
+    
+
+}
+
+function fade(element) {
+    var slideSource = document.getElementById(element);
+    slideSource.classList.toggle('fade');
 }
