33583895a13113276919fe167b02a55e7500a763
Florin97 <velesca.florin97@gmail.com>
2018-03-18 08:54:41 +0200

    Add files via upload

--- magicWebsite.js
+++ magicWebsite.js
@@ -1,13 +1,21 @@
 function setDisplayNone(firstId,secondId){
+
+    var transitionTime = 500;
+
     fade(firstId);
     
     setTimeout(function(){
         document.getElementById(firstId).style.display="none";
-        document.getElementById(secondId).style.display="block";
+        
+        if(secondId == "chooseCaracterCanvas")
+            document.getElementById(secondId).style.display="inline-block";
+        else
+            document.getElementById(secondId).style.display="block";
+
         setTimeout(function(){
             fade(secondId);
-        }, 1000);
-    }, 1000);
+        }, transitionTime);
+    }, transitionTime);
     
 
 }
