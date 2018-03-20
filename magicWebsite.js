a36491a0185a67819002fd3b0e026f7477790388
cristian1997 <cris90297@yahoo.co.uk>
2018-03-20 13:23:06 +0200

    Multiple transitions and menu changes

--- magicWebsite.js
+++ magicWebsite.js
@@ -1,8 +1,15 @@
 function sceneTransition(firstId, secondId) {
-  document.getElementById("leftDiv").classList.add('leftCurtain');
-  document.getElementById("rightDiv").classList.add('rightCurtain');
-  document.getElementById(firstId).classList.remove('visible');
-  document.getElementById(firstId).classList.add('invisible');
-  document.getElementById(secondId).classList.remove('invisible');
-  document.getElementById(secondId).classList.add('visible');
+  if(secondId == 'gameCanvas') {
+  	document.getElementById("leftDiv").classList.add('leftCurtain');
+	  document.getElementById("rightDiv").classList.add('rightCurtain');
+	  document.getElementById(firstId).classList.remove('visible');
+	  document.getElementById(firstId).classList.add('invisible');
+	  document.getElementById(secondId).classList.remove('invisible');
+	  document.getElementById(secondId).classList.add('visible');
+	} else {
+		document.getElementById(firstId).classList.remove('toTopTransition');
+	  document.getElementById(firstId).classList.add('toBotTransition');
+	  document.getElementById(secondId).classList.remove('toBotTransition');
+	  document.getElementById(secondId).classList.add('toTopTransition');
+	}
 }
