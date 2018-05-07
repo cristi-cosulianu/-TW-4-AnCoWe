b4917b24d0804d3887a138d6cde3f6e7f8be6ab4
Florin97 <velesca.florin97@gmail.com>
2018-05-07 16:51:45 +0300

    Create a blueprint for Database API
    Register, Login partial function implemented

--- server/sql.js
+++ server/sql.js
@@ -11,3 +11,26 @@ con.connect(function(err) {
   console.log("Connected!");
     
 });
+
+registerUser = function(user,password,con) {
+    
+    con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO Users (name, password,Score,Options) VALUES ('user', 'pass','0','right','left','down','jump')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("user register");
+  });
+});
+    
+}
+logInUser = function (user,con){
+if (err) throw err;
+    con.connect(function(err)) {
+                
+                }
+    var sql = "Select * from Users where name = '" + user + "'";
+    con.query(sql,)
+    
+}
