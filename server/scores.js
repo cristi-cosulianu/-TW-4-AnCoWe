67326ede1c88bca6c87004eb3d0d20fe81c2c2bc
Florin97 <velesca.florin97@gmail.com>
2018-05-10 20:47:10 +0300

    implement score db api

---
+++ server/scores.js
@@ -0,0 +1,36 @@
+sql = require('./sqlconnect.js');
+
+//console.log(sql.conn.query);
+//sql.conn.query("Select * from scores;",function (err, result, fields) {
+//    
+//    console.log(JSON.stringify(result));
+//    
+//});
+//console.log("after query");
+class ScoreController {
+    constructor() {
+        this.conn = sql.conn;
+    }
+    
+    static update(player, newScore) {
+        sql.conn.query("UPDATE scores SET score = " + newScore + " WHERE username = '" + player + "';");
+        sql.conn.end();
+    }
+    
+    static getScores(callback) {
+        sql.conn.query("Select * from scores;",function (err, result, fields) {
+            if(err){
+                  callback(err, "")
+            }else{
+                  callback("", JSON.stringify(result))
+            }
+      
+    });
+    }
+}
+ScoreController.getScores(function(err, result){
+        console.log(JSON.stringify(result));
+           
+        });
+// use
+ScoreController.update('loghin', 99999999);
