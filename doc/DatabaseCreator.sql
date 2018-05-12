99a4ab9421165187811a8157e459315a02ace815
Florin97 <velesca.florin97@gmail.com>
2018-05-12 19:05:25 +0300

    provide a script to create database and tables

---
+++ doc/DatabaseCreator.sql
@@ -0,0 +1,23 @@
+Create database twProj;
+Use twProj;
+CREATE TABLE scorest (id INT NOT NULL,
+                     username varchar(30) NOT NULL,
+                     score INT,
+                     FOREIGN KEY (id) REFERENCES Users(id)
+                    );
+
+CREATE TABLE Options (id INT NOT NULL,
+                     leftKey INT NOT NULL,
+                     rightKey INT NOT NULL,
+                     downKey INT NOT NULL,
+                     jumpKey INT NOT NULL,
+                     dashKey INT NOT NULL,
+                     musicVolume INT,
+                     soundVolume INT NOT NULL,
+                     FOREIGN KEY (id) REFERENCES Users(id)
+                     );
+CREATE TABLE Users (id INT NOT NULL,
+                    username varchar(30) NOT NULL,
+                    password varchar(30) NOT NULL,
+                    PRIMARY KEY (id)
+                    );                     
