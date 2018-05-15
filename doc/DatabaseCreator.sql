e091c10d6529c8fdfc3d424065ac3329e3dfb907
Florin97 <velesca.florin97@gmail.com>
2018-05-15 16:59:48 +0300

    another change on sql creator

--- doc/DatabaseCreator.sql
+++ doc/DatabaseCreator.sql
@@ -1,11 +1,16 @@
 Create database twProj;
 Use twProj;
+CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL,
+                    username varchar(32) NOT NULL,
+                    password varchar(32) NOT NULL,
+                    PRIMARY KEY (id)
+                    );
 CREATE TABLE scores (user_id INT NOT NULL,
                      score INT NOT NULL,
-                     FOREIGN KEY (user_id) REFERENCES Users(id)
+                     FOREIGN KEY (user_id) REFERENCES users(id)
                     );
 
-CREATE TABLE Options (user_id INT NOT NULL,
+CREATE TABLE options (user_id INT NOT NULL,
                      left_key INT NOT NULL,
                      right_key INT NOT NULL,
                      down_key INT NOT NULL,
@@ -13,14 +18,9 @@ CREATE TABLE Options (user_id INT NOT NULL,
                      dash_key INT NOT NULL,
                      music_volume INT NOT NULL,
                      sound_volume INT NOT NULL,
-                     FOREIGN KEY (user_id) REFERENCES Users(id)
+                     FOREIGN KEY (user_id) REFERENCES users(id)
                      );
-CREATE TABLE Users (id INT AUTO_INCREMENT NOT NULL,
-                    username varchar(32) NOT NULL,
-                    password varchar(32) NOT NULL,
-                    PRIMARY KEY (id)
-                    );
-CREATE TABLE sessions ( user_id INT NOT NULL,
+CREATE TABLE sessions (user_id INT NOT NULL,
                        session_id varchar(50) NOT NULL,
-                       FOREIGN KEY (user_id) REFERENCES Users(id)
+                       FOREIGN KEY (user_id) REFERENCES users(id)
                     );                    
