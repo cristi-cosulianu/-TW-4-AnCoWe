933454c7e252890250c3422cbb665f42332d8a15
Florin97 <velesca.florin97@gmail.com>
2018-05-15 15:35:36 +0300

    Change database script

--- doc/DatabaseCreator.sql
+++ doc/DatabaseCreator.sql
@@ -1,22 +1,26 @@
 Create database twProj;
 Use twProj;
-CREATE TABLE scores (userId INT NOT NULL,
+CREATE TABLE scores (user_id INT NOT NULL,
                      score INT NOT NULL,
-                     FOREIGN KEY (id) REFERENCES Users(id)
+                     FOREIGN KEY (user_id) REFERENCES Users(id)
                     );
 
-CREATE TABLE Options (userId INT NOT NULL,
-                     leftKey INT NOT NULL,
-                     rightKey INT NOT NULL,
-                     downKey INT NOT NULL,
-                     jumpKey INT NOT NULL,
-                     dashKey INT NOT NULL,
-                     musicVolume INT NOT NULL,
-                     soundVolume INT NOT NULL,
-                     FOREIGN KEY (userId) REFERENCES Users(id)
+CREATE TABLE Options (user_id INT NOT NULL,
+                     left_key INT NOT NULL,
+                     right_key INT NOT NULL,
+                     down_key INT NOT NULL,
+                     jump_key INT NOT NULL,
+                     dash_key INT NOT NULL,
+                     music_volume INT NOT NULL,
+                     sound_volume INT NOT NULL,
+                     FOREIGN KEY (user_id) REFERENCES Users(id)
                      );
-CREATE TABLE Users (id INT NOT NULL,
-                    username varchar(30) NOT NULL,
-                    password varchar(30) NOT NULL,
+CREATE TABLE Users (id INT AUTO_INCREMENT NOT NULL,
+                    username varchar(32) NOT NULL,
+                    password varchar(32) NOT NULL,
                     PRIMARY KEY (id)
                     );
+CREATE TABLE sessions ( user_id INT NOT NULL,
+                       session_id varchar(50) NOT NULL,
+                       FOREIGN KEY (user_id) REFERENCES Users(id)
+                    );                    
