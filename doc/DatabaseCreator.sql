64b791de2e5d8bfab4013d15b5c6ad3381d799d3
Florin97 <velesca.florin97@gmail.com>
2018-05-15 14:23:55 +0300

    Add files via upload

--- doc/DatabaseCreator.sql
+++ doc/DatabaseCreator.sql
@@ -1,23 +1,22 @@
 Create database twProj;
 Use twProj;
-CREATE TABLE scores (id INT NOT NULL,
-                     username varchar(30) NOT NULL,
-                     score INT,
+CREATE TABLE scores (userId INT NOT NULL,
+                     score INT NOT NULL,
                      FOREIGN KEY (id) REFERENCES Users(id)
                     );
 
-CREATE TABLE Options (id INT NOT NULL,
+CREATE TABLE Options (userId INT NOT NULL,
                      leftKey INT NOT NULL,
                      rightKey INT NOT NULL,
                      downKey INT NOT NULL,
                      jumpKey INT NOT NULL,
                      dashKey INT NOT NULL,
-                     musicVolume INT,
+                     musicVolume INT NOT NULL,
                      soundVolume INT NOT NULL,
-                     FOREIGN KEY (id) REFERENCES Users(id)
+                     FOREIGN KEY (userId) REFERENCES Users(id)
                      );
 CREATE TABLE Users (id INT NOT NULL,
                     username varchar(30) NOT NULL,
                     password varchar(30) NOT NULL,
                     PRIMARY KEY (id)
-                    );                     
+                    );
