f55448cf752fba0017ddd202a9fbc6d79d8de24f
Velesca Florin <Velesca.florin97@gmail.com>
2018-05-28 14:11:01 +0300

    reduce GET request costs

--- server/marvel.js
+++ server/marvel.js
@@ -22,7 +22,7 @@ module.exports = {
 	}
 };
 
-getIcon = function(heroname) {
+getIconStory = function(heroname) {
   
   var url = "https://gateway.marvel.com/v1/public/characters?apikey=15b0df9dd78ed4c3d58e10b0c3d36a57&hash=758e48b905e396fca02324d24f1f7b06&ts=432&name=" + heroname;
   
@@ -30,39 +30,51 @@ getIcon = function(heroname) {
   
   var thumbnail = JSON.parse(res.getBody()).data.results[0].thumbnail;
   var imgUrl = thumbnail.path + '/portrait_xlarge.' + thumbnail.extension;
-
-	return { code: 200, message: imgUrl };
-};
-getStory = function(heroname) {
-  
-  var url = "https://gateway.marvel.com/v1/public/characters?apikey=15b0df9dd78ed4c3d58e10b0c3d36a57&hash=758e48b905e396fca02324d24f1f7b06&ts=432&name=" + heroname;
-  
-  var res = request('GET', url);
-  
   var story = JSON.parse(res.getBody()).data.results[0].description;
-  //var story = thumbnail.path + '/portrait_xlarge.' + thumbnail.extension;
+  var img_story = {};
+  img_story['imgUrl'] = imgUrl;
+  img_story['story'] = story;	
 
-	return { code: 200, message: story };
+
+
+	return { code: 200, message: img_story };
 };
+// getStory = function(heroname) {
+  
+//   var url = "https://gateway.marvel.com/v1/public/characters?apikey=15b0df9dd78ed4c3d58e10b0c3d36a57&hash=758e48b905e396fca02324d24f1f7b06&ts=432&name=" + heroname;
+  
+//   var res = request('GET', url);
+  
+//   var story = JSON.parse(res.getBody()).data.results[0].description;
+//   //var story = thumbnail.path + '/portrait_xlarge.' + thumbnail.extension;
+
+// 	return { code: 200, message: story };
+// };
 
 
-getIconAnime = function (id) {
+getIconStoryAnime = function (id) {
     var url = "https://api.jikan.moe/character/" + id + "/pictures";
     var res = request('GET',url);
     var imgUrl = JSON.parse(res.getBody()).image_url;
-    return { code: 200, message: imgUrl };
-
-}
-getStoryAnime = function (id) {
-    var url = "https://api.jikan.moe/character/" + id + "/pictures";
-    var res = request('GET',url);
     var story = JSON.parse(res.getBody()).about;
-    return { code: 200, message: story };
+    //var img_story = imgUrl.merge(story);
+    var img_story = {};
+	img_story['imgUrl'] = imgUrl;
+	img_story['story'] = story;
+    return { code: 200, message: img_story };
 
 }
+// getStoryAnime = function (id) {
+//     var url = "https://api.jikan.moe/character/" + id + "/pictures";
+//     var res = request('GET',url);
+//     var story = JSON.parse(res.getBody()).about;
+//     return { code: 200, message: story };
+
+// }
 getAnimeCharacters = function () {
     var characters = "[\r\n  {\r\n    \"image_url\": \"https:\\\/\\\/myanimelist.cdn-dena.com\\\/r\\\/46x64\\\/images\\\/characters\\\/9\\\/69275.jpg?s=1e1ccda911e91eac0afa621fcffc2621\",\r\n    \"mal_id\": 145,\r\n    \"url\": \"https:\\\/\\\/myanimelist.net\\\/character\\\/145\\\/Sakura_Haruno\",\r\n    \"name\": \"Haruno, Sakura\"\r\n  },\r\n  {\r\n    \"image_url\": \"https:\\\/\\\/myanimelist.cdn-dena.com\\\/r\\\/46x64\\\/images\\\/characters\\\/9\\\/69275.jpg?s=1e1ccda911e91eac0afa621fcffc2621\",\r\n    \"mal_id\": 145,\r\n    \"url\": \"https:\\\/\\\/myanimelist.net\\\/character\\\/145\\\/Sakura_Haruno\",\r\n    \"name\": \"Haruno, Sakura\"\r\n  }\r\n]";
 
     return { code: 200, message: characters };
 }
-//console.log(getStory("Hulk"));
+
+
